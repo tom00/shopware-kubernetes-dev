476314a2e0656167aa22a66a3cf8b67769eaf6b5
Tomasz Gajewski <tgajewski@gmail.com>
2024-12-02 12:45:02 +0100

    Used single vm-driver - docker for all platforms

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 
-OS_NAME=$(uname -s | tr A-Z a-z)
-ARCH=$(uname -m | tr A-Z a-z)
-
-if [[ "${OS_NAME}" == "darwin" && "${ARCH}" != "arm64" ]]; then
-	VM="hyperkit"
-elif [ "${OS_NAME}" == "linux" ]; then
-	VM="kvm2"
-elif [[ "${OS_NAME}" == "darwin" && "${ARCH}" == "arm64" ]]; then
-  VM="docker"
-else
-	echo "Your operating system is not yet supported."
-	exit 1
-fi
+VM=docker
+CPUS=4
+MEMORY=7G
 
 minikube start \
 	--kubernetes-version=v1.31.0 \
 	--vm-driver=${VM} \
-	--cpus=4 \
-	--memory=7G \
+	--cpus=${CPUS} \
+	--memory=${MEMORY} \
 	--disk-size=30G \
 	--network-plugin=cni --cni=calico
 	minikube addons enable default-storageclass
